80fae44dec761cd5bdc4fb2fa44991b331c208c3
ScorpioGK <scorpiogameking@gmail.com>
2026-02-12 17:44:27 -0500

    Added remove_interface

--- source/window_manager/_gui_manager.py
+++ source/window_manager/_gui_manager.py
@@ -8,6 +8,9 @@ class GUIManager():
     def add_interface(self, interface):
         self.interfaces.update({interface.title:interface})
     
+    def remove_interface(self, interface):
+        self.interfaces.pop(interface.title)
+    
     def render(self):
         for i in self.interfaces:
             self.interfaces[i].render()
